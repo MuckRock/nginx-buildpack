e55b5000185c20f3468421d9cd29cb5a22d24de9
mitchelljkotler <mitch@mitchellkotler.net>
2015-07-10 09:39:44 -0400

    Update build_nginx.sh
    add real ip module

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -41,6 +41,7 @@ echo "Downloading $headers_more_nginx_module_url"
 		--with-pcre=pcre-${PCRE_VERSION} \
 		--prefix=/tmp/nginx \
 		--add-module=/${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION}
+		--with-http_realip_module
 	make install
 )
 
